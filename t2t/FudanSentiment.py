d84e0f948e22ab742dd85f9a8e4c792bb761d803
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-03 23:37:50 +0000

    Adding numeric_owner as keyword arguement

--- t2t/FudanSentiment.py
+++ t2t/FudanSentiment.py
@@ -46,7 +46,7 @@ class FudanSentiment(SentimentIMDB):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, tmp_dir)
